511015aa2e7fdca3df8eb93f07aa47e9cf9eb9c0
Jere Laine <jere.lainee02@gmail.com>
2023-05-02 23:00:47 +0300

    Get all events - HTTPException added

--- app/models/crud_events.py
+++ app/models/crud_events.py
@@ -1,9 +1,15 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from . import models, schemas
+
+event_types = ["level_started", "level_solved"]
 
 def read_events(db: Session, event_type: str):
     if event_type == None:
         return db.query(models.Events).all()
+    elif event_type not in event_types:
+        raise HTTPException(
+            status_code=400, detail="Invalid event type")
     else: 
         return db.query(models.Events).filter(models.Events.type == event_type).all()
     
