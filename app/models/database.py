656032c52b8fb4bbc288c222a9632757f624209e
Jere Laine <jere.lainee02@gmail.com>
2023-05-02 15:46:00 +0300

    Player and Events models, base implementation done

--- app/models/database.py
+++ app/models/database.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_playerapi.db"
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base = declarative_base()
+Base = declarative_base()
---
+++ app/models/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    events = relationship("events")
+
+class Events(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(String)
+    detail = Column(String)
+    timestamp = Column(DateTime, default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) # not sure about this
+    player_id = Column(Integer, ForeignKey("players.id"))
